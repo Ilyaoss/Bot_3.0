212a3ec982bebb9255053ac8fe553a4de0b69699
Ilusha <Ilyao2012@yandex.ru>
2018-08-13 14:21:56 +0300

    check this

--- index.php
+++ index.php
@@ -1,31 +1,18 @@
-<?php 
-
+<?php
 require_once './vendor/autoload.php';
-
+const VK_TOKEN = 'ae51ff9f59f0c40f029c67c3588466bcec1f7ff0f333404769952a31290c3cf097f5b7c0cfd90b0da4562';
 use VK\Client\Enums\VKLanguage;
 use VK\Client\VKApiClient;
-
-if (!isset($_REQUEST)) { 
-return; 
-} 
-
-//Строка для подтверждения адреса сервера из настроек Callback API 
-$confirmation_token = 'd18ce045'; 
-
-//Ключ доступа сообщества 
-const VK_TOKEN = '0f0567f6ffa539268e0b6558d7622d375e6232283542932eadc135443d88109330c37b64bbb8c26bf525a'; 
-
-const VERSION = 5.80;
-
+function myLog($str) {
+    file_put_contents("php://stdout", "$str\n");
+}
 const COLOR_NEGATIVE = 'negative';
 const COLOR_POSITIVE = 'positive';
 const COLOR_DEFAULT = 'default';
 const COLOR_PRIMARY = 'primary';
-
 const CMD_ID = 'ID';
 const CMD_NEXT = 'NEXT';
-const CMD_QUEST = 'QUEST';
-
+const CMD_TYPING = 'TYPING';
 function getBtn($label, $color, $payload = '') {
     return [
         'action' => [
@@ -36,88 +23,55 @@ function getBtn($label, $color, $payload = '') {
         'color' => $color
     ];
 }
-
-function myLog($str) {
-    file_put_contents("php://stdout", "$str\n");
-}
-
-//Получаем и декодируем уведомление 
-$data = json_decode(file_get_contents('php://input')); 
-$vk = new VKApiClient('5.5', VKLanguage::RUSSIAN);
-
-//Проверяем, что находится в поле "type" 
-switch ($data->type) { 
-
-//Если это уведомление для подтверждения адреса... 
-case 'confirmation': 
-	//...отправляем строку для подтверждения 
-	echo $confirmation_token; 
-	break; 
-
-//Если это уведомление о новом сообщении... 
-case 'message_new': 
-	//...получаем id его автора 
-	$message = $data['object'] ?? [];
-    $user_id = $message['user_id'] ?? 0;
+$json = file_get_contents('php://input');
+//myLog($json);
+$data = json_decode($json, true);
+$type = $data['type'] ?? '';
+$vk = new VKApiClient('5.78', VKLanguage::RUSSIAN);
+if ($type === 'message_new') {
+    $message = $data['object'] ?? [];
+    $userId = $message['user_id'] ?? 0;
     $body = $message['body'] ?? '';
     $payload = $message['payload'] ?? '';
-	
-	//затем с помощью users.get получаем данные об авторе 
-	$user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&access_token={".VK_TOKEN."}&v=5.5")); 
-
-	//и извлекаем из ответа его имя 
-	$user_name = $user_info->response[0]->first_name; 
-
-	
-	if ($payload) {
+    if ($payload) {
         $payload = json_decode($payload, true);
     }
-	
-	myLog("MSG: ".$body." PAYLOAD:".$payload);
-	
-	$kbd = [
-		one_time => false,
-		'buttons' => [
-				[getBtn("Покажи мой ID", COLOR_DEFAULT, CMD_ID)],
-				[getBtn("Далее", COLOR_PRIMARY, CMD_NEXT)],
-			]
-	];
-	
-	$msg = "Привет я бот!";
-	
-	//нестрогое == не забудь!
-	switch(payload){
-		case CMD_ID:
-			$msg = "Ваш id ".$userId;
-			break;
-		case CMD_NEXT:
-			$kbd = [
-				'one_time' => false,
-				'buttons' => [
-					[getBtn("Как дела?", COLOR_POSITIVE, CMD_QUEST)],
-					[getBtn("Назад", COLOR_NEGATIVE)],
-				]
-			];
-			break;
-		case CMD_QUEST:
-			$msg = "Отлично, спасибо! А у тебя как?";
-			break;
-	}
-		
-		
-	//С помощью messages.send отправляем ответное сообщение 
-	/*$request_params = array( 
-	'message' => "Hello, {$user_name}!", 
-	'user_id' => $user_id, 
-	'access_token' => VK_TOKEN, 
-	'v' => VERSION 
-	); */
-
-	
-	try {
+    myLog("MSG: ".$body." PAYLOAD:".$payload);
+    $kbd = [
+        'one_time' => false,
+        'buttons' => [
+            [getBtn("Покажи мой ID", COLOR_DEFAULT, CMD_ID)],
+            [getBtn("Далее", COLOR_PRIMARY, CMD_NEXT)],
+        ]
+    ];
+    $msg = "Привет я бот!";
+    if ($payload === CMD_ID) {
+        $msg = "Ваш id ".$userId;
+    }
+    if ($payload === CMD_NEXT) {
+        $kbd = [
+            'one_time' => false,
+            'buttons' => [
+                [getBtn("Пошли тайпинг", COLOR_POSITIVE, CMD_TYPING)],
+                [getBtn("Назад", COLOR_NEGATIVE)],
+            ]
+        ];
+    }
+    if ($payload === CMD_TYPING) {
+        try {
+            $res = $vk->messages()->setActivity(VK_TOKEN, [
+                'peer_id' => $userId,
+                'type' => 'typing'
+            ]);
+            $msg = null;
+        } catch (\Exception $e) {
+            myLog( $e->getCode().' '.$e->getMessage() );
+        }
+    }
+    try {
         if ($msg !== null) {
             $response = $vk->messages()->send(VK_TOKEN, [
-                'peer_id' => $user_id,
+                'peer_id' => $userId,
                 'message' => $msg,
                 'keyboard' => json_encode($kbd, JSON_UNESCAPED_UNICODE)
             ]);
@@ -125,16 +79,5 @@ case 'message_new':
     } catch (\Exception $e) {
         myLog( $e->getCode().' '.$e->getMessage() );
     }
-
-	/*$get_params = http_build_query($request_params); 
-
-	file_get_contents('https://api.vk.com/method/messages.send?'. $get_params); */
-
-	//Возвращаем "ok" серверу Callback API 
-
-	echo('ok'); 
-
-	break; 
-
-} 
-?> 
+}
+echo  "OK";
