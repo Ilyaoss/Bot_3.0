43d38258ec1601dcf2a2032ace3f2e76565dee5f
Ilusha <Ilyao2012@yandex.ru>
2018-08-15 17:35:08 +0300

    test img

--- index.php
+++ index.php
@@ -116,9 +116,9 @@ switch ($type) {
 					$myCurl = curl_init();
 					//$file = 'https://s.fishki.net/upload/users/2017/04/05/414721/8419b6ac67d83d3dea58db13a67b2763.jpg';
 					//$file = curl_file_create($file, mime_content_type($file), pathinfo($file)['basename']);
-					$f=new CURLFile('test.jpg','image/jpeg','test');
+					$f=curl_file_create('test.jpg','image/jpeg','test');
 					myLog($f->getFilename());
-					$data_file = ['file'=> $f];
+					$data_file = ['photo'=> $f];
 					curl_setopt_array($myCurl, array(
 						CURLOPT_URL => $url['upload_url'],
 						CURLOPT_HTTPHEADER=>['Content-Type: multipart/form-data'],
