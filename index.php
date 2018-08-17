a9958ab579f6ebf05cbc33e086679da5119369c4
Ilusha <Ilyao2012@yandex.ru>
2018-08-17 13:35:24 +0300

    test

--- index.php
+++ index.php
@@ -73,7 +73,7 @@ for($i=1;$i<count($def_mas);++$i) {
 }
 $keys_1 = array_column($array, 0); 
 $keys_2 = array_unique(array_column($def_mas, 1),SORT_REGULAR);
-$keys_3 = array_column($def_mas, 2);
+$keys_3 = array_column($array, 2);
 myLog("Keys: ".json_encode($keys_1,JSON_UNESCAPED_UNICODE));
 //myLog("Array: ".json_encode($array),JSON_UNESCAPED_UNICODE);
 myLog("Test: $array[0]\n count: ".count($array)."\n c1 ".$array['Комм и маркетинг']['Медиа'][0]);
