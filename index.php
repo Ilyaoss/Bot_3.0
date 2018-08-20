677fddd89acda908ae984c9a5b34a48161f777fd
Ilusha <Ilyao2012@yandex.ru>
2018-08-20 13:02:24 +0300

    test

--- index.php
+++ index.php
@@ -373,13 +373,17 @@ switch ($type) {
 							array_push($buttons,[getBtn('В главное меню', COLOR_NEGATIVE,CMD_MAIN),getBtn('Далее-->', COLOR_POSITIVE,CMD_NEXT)]);
 						}/*------------Закончил Дублировать код----------*/
 						/* */
-						elseif($payload[$key[0]]==CMD_NEXT)
+						elseif($payload[$key[0]]== CMD_NEXT)
 						{
 							$keys_2 = array_keys($array[$key[0]]);
 							$buttons = getKbd_3(7,count($keys_2),$keys_2,$payload);//count($keys_2)
 							array_push($buttons,[getBtn('Подписаться на всё', COLOR_PRIMARY,[$key[0]=>'SUBS_ALL'])]);
 							array_push($buttons,[getBtn('<-- На пред. стр.', COLOR_NEGATIVE,$key[0]),getBtn('В главное меню', COLOR_NEGATIVE,CMD_MAIN)]);
 							array_push($buttons,[getBtn('Назад', COLOR_NEGATIVE,CMD_BACK)]);
+							$kbd = [
+								'one_time' => false,
+								'buttons' => $buttons
+							];
 						}
 						/*3-й уровень кнопок:*/
 						else{
