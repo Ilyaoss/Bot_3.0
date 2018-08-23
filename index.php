b22d6aa22175ce65e303468f7044e76e545efcf6
Ilusha <Ilyao2012@yandex.ru>
2018-08-23 17:59:00 +0300

    test

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ $confirmation_token = 'd18ce045';
 function getBtn($label, $color = COLOR_DEFAULT, $payload = '') {
 	if(strlen($label)>MAX_LENGHT)
 	{
-		$start = MAX_LENGHT/2 - 6;
+		$start = MAX_LENGHT/2 - 8;
 		$end = MAX_LENGHT/2 + 1;
 		myLog("Lab bef: $label count:".strlen($label));
 		$label = substr($label,0,$start).'.. ..'.substr($label,-(MAX_LENGHT-$end));
