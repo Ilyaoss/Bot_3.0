9aac2d7ca5a153e2b5ece23c7599b6456ea3b64b
Ilusha <Ilyao2012@yandex.ru>
2018-08-23 11:18:45 +0300

    test

--- index.php
+++ index.php
@@ -36,12 +36,11 @@ $confirmation_token = 'd18ce045';
 function getBtn($label, $color = COLOR_DEFAULT, $payload = '') {
 	if(strlen($label)>MAX_LENGHT)
 	{
-		$start = MAX_LENGHT/2-3;
-		$end = MAX_LENGHT/2+3;
+		$start = MAX_LENGHT/2 - 3;
+		$end = MAX_LENGHT/2 + 3;
 		myLog("Lab bef: $label count:".strlen($label));
-		$label = substr($label,0,$start).'.. ..'.substr($label,-($MAX_LENGHT-$end));
+		$label = substr($label,0,$start).'.. ..'.substr($label,-(MAX_LENGHT-$end));
 		myLog("Lab aft: $label count:".strlen($label));
-		myLog(substr($label,-($MAX_LENGHT-$end)));
 	}
 	return [
         'action' => [
