2a834f9a5e86fdbb81e292e5d4ddf6bd3e29a3a8
Ilusha <Ilyao2012@yandex.ru>
2018-08-29 14:36:14 +0300

    add db

--- functions.php
+++ functions.php
@@ -11,14 +11,14 @@ function connect_db() {
 
     $link = mysqli_connect($server, $username, $password,$db);
 	if (!$link) {
-		echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
-		echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
-		echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
+		myLog( "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL);
+		myLog( "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL);
+		myLog( "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL);
 		exit;
 	}
 
-	echo "Соединение с MySQL установлено!" . PHP_EOL;
-	echo "Информация о сервере: " . mysqli_get_host_info($link) . PHP_EOL;
+	myLog( "Соединение с MySQL установлено!" . PHP_EOL);
+	myLog( "Информация о сервере: " . mysqli_get_host_info($link) . PHP_EOL);
 	if ($link->connect_errno) {
 		myLog("Не удалось подключиться к MySQL: (" . $link->connect_errno . ") " . $link->connect_error);
 	}
